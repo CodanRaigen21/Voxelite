7cb1f9dedd8089a5038c93653857b815be47aacd
CodanRaigen21 <codanraigen21@gmail.com>
2025-11-16 01:49:43 +0400

    changes

--- scripts/news.js
+++
@@ -1,9 +0,0 @@
-const tweets = [
-	`<blockquote class="twitter-tweet"><a href="https://twitter.com/real_cr21/status/1684543792144166914?ref_src=twsrc%5Etfw"></a></blockquote>`,
-	`<blockquote class="twitter-tweet"><a href="https://twitter.com/real_cr21/status/1686259382919614464?ref_src=twsrc%5Etfw"></a></blockquote>`,
-	`<blockquote class="twitter-tweet"><a href="https://twitter.com/real_cr21/status/1689763903268392960?ref_src=twsrc%5Etfw"></a></blockquote>`
-]
-
-tweets.forEach(t =>{
-	document.querySelector('.contentHolder').innerHTML += t;
-})
